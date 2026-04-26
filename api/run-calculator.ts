abc7968c8d5d3c7b9df1c59643b0ca135ef61f69
Jaysnsane <jaysnsane1975@gmail.com>
2026-04-26 02:51:05 -0500

    Update run-calculator.ts

--- api/run-calculator.ts
+++ api/run-calculator.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { calculateSolarBatteryDesign } from '../src/solarBatteryCalculator';
+import { calculateSolarBatteryDesign } from '../src/solarBatteryCalculator.ts';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
